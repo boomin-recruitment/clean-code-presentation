61532a4a1ed7f9ca0439313f997a30b965bc052e
Tom Nicklin <tom.nicklin@launchb.co.uk>
2020-02-05 13:28:33 +0000

    Remove more magic numbers - another approach

--- example.cs
+++ example.cs
@@ -6,27 +6,35 @@ public enum AccountStatus
     MostValuableCustomer = 4
 }
 
+public static class Constants
+{
+    public const int MAXIMUM_DISCOUNT_FOR_LOYALTY = 5;
+    public const decimal DISCOUNT_FOR_SIMPLE_CUSTOMERS = 0.1m;
+    public const decimal DISCOUNT_FOR_VALUABLE_CUSTOMERS = 0.3m;
+    public const decimal DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS = 0.5m;
+}
+
 public class DiscountManager
 {
     public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
     {
         decimal priceAfterDiscount = 0;
-        decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > 5) ? (decimal)5 / 100 : (decimal)timeOfHavingAccountInYears / 100;
+        decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY) ? (decimal)Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY / 100 : (decimal)timeOfHavingAccountInYears / 100;
         switch (accountStatus)
         {
             case AccountStatus.NotRegistered:
                 priceAfterDiscount = price;
                 break;
             case AccountStatus.SimpleCustomer:
-                priceAfterDiscount = (price - (0.1m * price));
+                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_SIMPLE_CUSTOMERS * price));
                 priceAfterDiscount = priceAfterDiscount - (discountForLoyaltyInPercentage * priceAfterDiscount);
                 break;
             case AccountStatus.ValuableCustomer:
-                priceAfterDiscount = (price - (0.3m * price));
+                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_VALUABLE_CUSTOMERS * price));
                 priceAfterDiscount = priceAfterDiscount - (discountForLoyaltyInPercentage * priceAfterDiscount);
                 break;
             case AccountStatus.MostValuableCustomer:
-                priceAfterDiscount = (price - (0.5m * price));
+                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS * price));
                 priceAfterDiscount = priceAfterDiscount - (discountForLoyaltyInPercentage * priceAfterDiscount);
                 break;
             default:
