89f86db91685045eb210ab35a96c0f4ba6177384
Tom Nicklin <tom.nicklin@launchb.co.uk>
2020-02-05 13:37:38 +0000

    Don't repeat yourself

--- example.cs
+++ example.cs
@@ -14,28 +14,41 @@ public static class Constants
     public const decimal DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS = 0.5m;
 }
 
+public static class PriceExtensions
+{
+    public static decimal ApplyDiscountForAccountStatus(this decimal price, decimal discountSize)
+    {
+        return price - (discountSize * price);
+    }
+
+    public static decimal ApplyDiscountForTimeOfHavingAccount(this decimal price, int timeOfHavingAccountInYears)
+    {
+        decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY) ? (decimal)Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY / 100 : (decimal)timeOfHavingAccountInYears / 100;
+        return price - (discountForLoyaltyInPercentage * price);
+    }
+}
+
 public class DiscountManager
 {
     public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
     {
         decimal priceAfterDiscount = 0;
-        decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY) ? (decimal)Constants.MAXIMUM_DISCOUNT_FOR_LOYALTY / 100 : (decimal)timeOfHavingAccountInYears / 100;
         switch (accountStatus)
         {
             case AccountStatus.NotRegistered:
                 priceAfterDiscount = price;
                 break;
             case AccountStatus.SimpleCustomer:
-                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_SIMPLE_CUSTOMERS * price));
-                priceAfterDiscount = priceAfterDiscount - (discountForLoyaltyInPercentage * priceAfterDiscount);
+                priceAfterDiscount = price.ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_SIMPLE_CUSTOMERS)
+                .ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
                 break;
             case AccountStatus.ValuableCustomer:
-                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_VALUABLE_CUSTOMERS * price));
-                priceAfterDiscount = priceAfterDiscount - (discountForLoyaltyInPercentage * priceAfterDiscount);
+                priceAfterDiscount = price.ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_VALUABLE_CUSTOMERS)
+                .ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
                 break;
             case AccountStatus.MostValuableCustomer:
-                priceAfterDiscount = (price - (Constants.DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS * price));
-                priceAfterDiscount = priceAfterDiscount - (discountForLoyaltyInPercentage * priceAfterDiscount);
+                priceAfterDiscount = price.ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS)
+                .ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
                 break;
             default:
                 throw new NotImplementedException();
