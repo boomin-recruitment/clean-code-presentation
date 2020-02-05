6fb7404713a3fb02a35ff16ec017ce07f37a287f
Tom Nicklin <tom.nicklin@launchb.co.uk>
2020-02-05 11:55:41 +0000

    Changing to better names.

--- example.cs
+++ example.cs
@@ -1,25 +1,26 @@
-public class Class1
+public class DiscountManager
 {
-  public decimal Calculate(decimal amount, int type, int years)
-  {
-    decimal result = 0;
-    decimal disc = (years > 5) ? (decimal)5/100 : (decimal)years/100; 
-    if (type == 1)
-    {
-      result = amount;
-    }
-    else if (type == 2)
-    {
-      result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
-    }
-    else if (type == 3)
-    {
-      result = (0.7m * amount) - disc * (0.7m * amount);
-    }
-    else if (type == 4)
-    {
-      result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
-    }
-    return result;
-  }
+  public decimal ApplyDiscount(decimal price, int accountStatus, int timeOfHavingAccountInYears)
+  {
+    decimal priceAfterDiscount = 0;
+    decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > 5) ? (decimal)5/100 : (decimal)timeOfHavingAccountInYears/100; 
+    if (accountStatus == 1)
+    {
+      priceAfterDiscount = price;
+    }
+    else if (accountStatus == 2)
+    {
+      priceAfterDiscount = (price - (0.1m * price)) - (discountForLoyaltyInPercentage * (price - (0.1m * price)));
+    }
+    else if (accountStatus == 3)
+    {
+      priceAfterDiscount = (0.7m * price) - (discountForLoyaltyInPercentage * (0.7m * price));
+    }
+    else if (accountStatus == 4)
+    {
+      priceAfterDiscount = (price - (0.5m * price)) - (discountForLoyaltyInPercentage * (price - (0.5m * price)));
+    }
+ 
+    return priceAfterDiscount;
+  }
 }
